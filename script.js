93eff0d16fb4f9a1bdb4d80b295b4b41dfc0c288
Nathan Stanford Sr <nathan.stanford@gmail.com>
2017-09-07 11:28:10 -0400

    Added your first Controller named MainController. Moved the Hello Beginner message into the scope in the Controller code.

--- script.js
+++ script.js
@@ -1,30 +1,6 @@
 // JavaScript Code goes here
-var createWorker = function() {
-    
-     var workCount = 0;
-    
-     var task1 = function(){
-       workCount +=1;
-       console.log("task1 " + workCount);
-     }
-   
-     var task2 = function(){
-       workCount +=1;
-       console.log("task2 " + workCount);
-     }
-    
-     return {
-       job1: task1,
-       job2: task2
-     };
-   
-   };
-   
-   var worker = createWorker();
-   
-   worker.job1();
-   worker.job2();
-   worker.job2();
-   worker.job2();
-   
-   
+var MainController = function($scope) {
+
+    $scope.message = "Hello Beginner!";
+
+}
