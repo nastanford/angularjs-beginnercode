9be348bf961b0189c2252da7bc65e1c393fd69b0
Nathan Stanford Sr <Nathan.Stanford@gmail.com>
2017-09-09 19:08:48 -0400

    Moved the functions out of global scope by creating an IIFE. Created a module for githubViewer.

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-// JavaScript Code goes here
-var MainController = function($scope, $http) {
-
-    var onUserComplete = function(response){
-        $scope.user = response.data;
-    };
-
-    var onError = function(response){
-        $scope.error = "Could not fetch the user.";
-    }
-
-    $http.get("https://api.github.com/users/nastanford")
-         .then(onUserComplete, onError);
-
-    $scope.message = "Hello, Angular Beginner!";
-
-}
+(function() {
+    
+      var app = angular.module("githubViewer", []);
+    
+      var MainController = function($scope, $http) {
+    
+        var onUserComplete = function(response) {
+          $scope.user = response.data;
+        };
+    
+        var onError = function(response) {
+          $scope.error = "Could not fetch the user.";
+        };
+    
+        $http.get("https://api.github.com/users/nastanford")
+          .then(onUserComplete, onError);
+    
+        $scope.message = "Hello, Angular Beginner!";
+    
+      };
+    
+      app.controller("MainController", MainController);
+    
+    }());
