3e4e396fc15ed5d3634205d6e3df417f4123e4b2
Nathan Stanford Sr <Nathan.Stanford@gmail.com>
2017-09-10 09:15:21 -0400

    Updated to seperate the user details to a different file and have  to use the avatar now that  the gravatar is been removed.

--- script.js
+++ script.js
@@ -1,24 +1,56 @@
 (function() {
-    
-      var app = angular.module("githubViewer", []);
-    
-      var MainController = function($scope, $http) {
-    
-        var onUserComplete = function(response) {
-          $scope.user = response.data;
+
+    var app = angular.module("githubViewer", []);
+
+    var MainController = function(
+        $scope, github, $interval, 
+        $log, $anchorScroll, $location) {
+
+        var onUserComplete = function(data) {
+            $scope.user = data;
+            github.getRepos($scope.user).then(onRepos, onError);
         };
-    
-        var onError = function(response) {
-          $scope.error = "Could not fetch the user.";
+
+        var onRepos = function(data) {
+            $scope.repos = data;
+            $location.hash("userDetails");
+            $anchorScroll();
         };
-    
-        $http.get("https://api.github.com/users/nastanford")
-          .then(onUserComplete, onError);
-    
-        $scope.message = "Hello, Angular Beginner!";
-    
-      };
-    
-      app.controller("MainController",["$scope","$http",MainController]);
-    
-    }());
+
+        var onError = function(reason) {
+            $scope.error = "Could not fetch the data.";
+        };
+
+        var decrementCountdown = function(){
+            $scope.countdown -= 1;
+            if($scope.countdown < 1){
+                $scope.search($scope.username);
+            }
+        };
+
+        var countdownInterval = null;
+        var startCountdown = function(){
+            countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+        };
+
+        $scope.search = function(username) {
+            $log.info("Searching for " + username);
+            github.getUser(username).then(onUserComplete, onError);
+            if(countdownInterval)    {
+                $interval.cancel(countdownInterval);
+                $scope.countdown = null;
+            }
+        };
+
+        $scope.username = "angular";
+        $scope.message = "GitHub Viewer";
+        $scope.repoSortOrder = "-stargazers_count";
+        $scope.countdown = 5;
+        startCountdown();
+
+
+    };
+
+    app.controller("MainController", MainController);
+
+}());
